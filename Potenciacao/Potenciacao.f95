8a993875b71c672de5d3a1270270578b042af757
adrianomoda <adrianomodafeitosa@gmail.com>
2018-03-18 01:16:56 -0300

    Potenciacao

---
+++ Potenciacao/Potenciacao.f95
@@ -0,0 +1,9 @@
+program main
+  integer base, expoente
+
+  write(*,*) "Entre com o valor da Base"
+  read*, base
+  write(*,*) "Entre com o valor do Expoente"
+  read*, expoente
+  write(*,*) "Potencia: " , base**expoente
+end
